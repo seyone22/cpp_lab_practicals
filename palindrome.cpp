e8d0294e2967dab442bc09c78e827dec6164a8ae
Seyone Gunasingham PUB <61453289+seyone22@users.noreply.github.com>
2022-01-25 00:30:59 +0530

    finished method 1

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -11,14 +11,26 @@ string getInput(string descriptor) {
     return input;
 }
 
+bool methodOne(string input) {
+    string stringReverse;
+
+    for(int i = input.length() - 1; i >= 0; i--)
+        stringReverse.push_back(input[i]);
+
+    if(input == stringReverse)
+        return true;
+    else
+        return false;
+}
+
 int main() {
+    int j;
     cout << "This program will check if the entered value is a palindrome or not." << endl;
     string toCheck = getInput("Please enter a word to check.");
 
-    vector<char> palindromeTest;
+    bool methodOneOut = methodOne(toCheck);
 
-
-
+    cout << methodOneOut;
 
     return 0;
 }
